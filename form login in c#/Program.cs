e65b7c2aa482e957d30b8d08581a381c01075c1b
TOT <sosomakerone@gmail.com>
2024-10-25 15:14:36 +0300

    Add files via upload
    Practical application of the first twenty videos of the course

---
+++ form login in c#/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static float V { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string name,pass;
+            Console.WriteLine("enter your name: \n");
+            name = Console.ReadLine();
+            Console.WriteLine("enter your password: \n");
+            pass = Console.ReadLine();
+            if( (name == "Radwa") && (pass == "123456789")){
+
+              Console.WriteLine("wellcome, " + name);
+              Console.ReadKey();
+            }
+            else if( !(pass =="123456789"))
+            {
+                Console.WriteLine("password is not correct");
+               
+            }
+            else if (!(name == "Radwa"))
+            {
+                Console.WriteLine("user name is not correct");
+
+            }
+
+
+            Console.ReadKey();
+                
+               
+            
+        }
+    }
+}
---
+++ the loops in c#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excersise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int i=1;
+
+            do {
+                Console.WriteLine(i);
+                i++;
+            } while (i < 5);
+
+
+
+         Console.ReadKey();
+        }
+    }
+}
